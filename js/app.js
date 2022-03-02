4c2e5c4d88259a11f2c8eac285d79d23f447439e
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-03-02 19:27:19 +0600

    front design complete

--- js/app.js
+++ js/app.js
@@ -1,181 +1,180 @@
-// // Add preloader 
-// const spin = param => {
-//     document.getElementById('spinner').style.display = param;
-// }
-// // Load Data From Api Using Fetch 
-// const loadBooksData = async () => {
-//     const searchField = document.getElementById('search-input');
-//     const searchText = searchField.value;
-//     // Clear Search Input 
-//      searchInput.value = '';
-//     // Error Handle for Empty Search 
+// html section id 
+const searchResult = document.getElementById('search-result');
 
-//     // if (searchText === '') {
-//     //     const error = document.getElementById('count')
-//     //     document.getElementById('count').style.display='block'
-//     //     error.innerText = `
-//     //     Please write a Book Name in search box.`;
-//     //     document.getElementById('books-container').textContent = '';
-//     //     error.style.color = 'red';
-//     // }
-//     // else {
-//     //     spin('block');
-//     //     // Url Dynamic 
-//     //     const url = `https://openlibrary.org/search.json?q=${searchText}`
-//     //     const res = await fetch(url);
-//     //     const data = await res.json();
-//     //     showBooks(data.docs, data);
-//     //     // Clear Search Input 
-//     //     searchInput.value = '';
-//     // }
-//     const booksContainer = document.getElementById('books-container');
-//     // Clear Books Container Data 
-//     booksContainer.textContent = '';
-//     // Count Items 
-//     const count = document.getElementById('count');
-//     count.innerText = `
-//         Your Result: ${books.length} Out of ${data.numFound}
-//         `;
-//     count.style.color = 'green';
-//     if(searchText == ''){
-//         const error = document.getElementById('count')
-//         error.innerText = `
-//         Please write a Book Name in search box.`;
-//     }
-//     else {
-//         spin('block');
-//             // Url Dynamic 
-//             const url = `https://openlibrary.org/search.json?q=${searchText}`
-//         try{
-//             const res = await fetch(url);
-//                 const data = await res.json();
-//                 showBooks(data.docs, data);
-        
-//         }
-//         catch(error){
-//             document.getElementById('count').style.display = 'block';
-//         }
-        
+const phoneDetails = document.getElementById('phone-details');
+phoneDetails.className ='row align-items-center';
 
-//     }
+// get data by search
+const getSearch = () => {
+    document.getElementById('fill-error').style.display ='none';
+    document.getElementById('result-found').style.display= "none";
+    document.getElementById('not-found').style.display = "none";
+    document.getElementById('spinner-section').style.display='block';
+    searchResult.innerHTML = '';
 
-
-// }
-// const showBooks = (books, data) => {
-//     const booksContainer = document.getElementById('books-container');
-//     // Clear Books Container Data 
-//     booksContainer.textContent = '';
-//     // Count Items 
-//     const count = document.getElementById('count');
-//     count.innerText = `
-//         Your Result: ${books.length} Out of ${data.numFound}
-//         `;
-//     count.style.color = 'green';
-//     // Error Handle for No items in result 
-//     if (books.length === 0) {
-//         const error = document.getElementById('count');
-//         error.innerText = `
-//         Please write a valid Book Name.`;
-//         error.style.color = 'red';
-//     }
-//     else {
-//         books.forEach(book => {
-//             // Create Dynamically Div in UI 
-//             const div = document.createElement('div');
-//             div.classList.add('col');
-//             div.classList.add('card-style')
-//             div.innerHTML = `
-//                 <div class="card">
-//                     <img height= '350px' p-5 src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-//                     <div class="card-body" style="height:200px; overflow:hidden">
-//                         <h5 class="card-title"> <b>Book Name:</b> ${book.title}</h5>
-//                         <p><b>Author Name:</b> ${book.author_name ? book.author_name.slice(0, 2) : 'No Author'}</p>
-//                         <p><b>Publisher:</b> ${book.publisher ? book.publisher : 'No Publisher'}</p>
-
-//                         <p class="card-text"><b>Published year:</b> ${book.first_publish_year ? book.first_publish_year : 'No Published Year'}</p>
-//                     </div>
-//                 </div>
-//             `;
-//             booksContainer.appendChild(div);
-//         });
-
-//     }
-//     spin('none');
-
-// }
-
-
-
-// error message display none 
-document.getElementById('error-message').style.display='none'
-// spinner
-const spin = spiner => {
-    document.getElementById('spinner').style.display = spiner;
-}
-// Load Data From Api
-const searchBooks = async () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // Error for empty text 
-    if (searchText === '') {
-        const error = document.getElementById('error-message')
-        error.innerText = `
-        Please write a Book Name in search box.`;
-        document.getElementById('error-message').style.display='block'
-        document.getElementById('books-container').textContent = '';
+    //take input value
+    const searchInput = document.getElementById('search-input');
+    const searchValue = searchInput.value.toLowerCase();
+    // error check
+    if (searchValue === '') {
+        searchResult.innerHTML = '';
+        document.getElementById('not-found').style.display = "none";
+        document.getElementById('spinner-section').style.display='none';
+        document.getElementById('fill-error').style.display ='block';
     }
     else {
-        spin('block');
-        // Url
-        const url = `https://openlibrary.org/search.json?q=${searchText}`
-        const res = await fetch(url);
-        const data = await res.json();
-        displaySearchResult(data.docs, data);
-        // Clear Search Input field 
-        searchField.value = '';
-    }
+        document.getElementById('fill-error').style.display ='none';
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
+        .then(response => response.json())
+        .then(data => displayResult(data.data))
+    };
+    searchInput.value = '';
+};
 
-
-}
-const displaySearchResult = (books, data) => {
-    const booksContainer = document.getElementById('books-container');
-    // Clear books container 
-    booksContainer.textContent = '';
-    // Amount of item 
-    const count = document.getElementById('error-message');
-    count.innerText = `
-        Your Result: ${books.length} Out of ${data.numFound}
-        `;
-    count.style.color = 'green';
-    // Error for No items
-    if (books.length === 0) {
-        const error = document.getElementById('error-message');
-        error.innerText = `
-        Please write a valid Book Name.`;
-        document.getElementById('error-message').style.display='block'
+// display search result
+const displayResult = data => {
+    // search not found check
+    if (data.length !== 0) {
+        document.getElementById('not-found').style.display = "none";
+        document.getElementById('result-text').innerText =`${data.length}`;
+        document.getElementById('result-found').style.display= "block";
+        //search result
+        const topTwenty = data.filter(result => data.indexOf(result) < 20);
+        showDisplay(topTwenty,searchResult);
+        document.getElementById('spinner-section').style.display='none';
     }
     else {
-        books.forEach(book => {
-            // Diyanamicly show books
+        document.getElementById('not-found').style.display = "block";
+        document.getElementById('spinner-section').style.display='none';
+    }
+};
+
+
+// get phone details to api
+const getDetails = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayDetails(data.data))
+};
+
+// display phone details 
+const displayDetails = data => {
+    phoneDetails.innerHTML = '';
+    // title section
+    const div = document.createElement('div');
+    div.className = 'details-photo col-sm-6'
+    div.innerHTML = `
+        <div class="d-flex justify-content-center"><img class="w-50" src="${data.image}"></div>
+        <h1 class="text-center my-2">${data?.name || 'No Found'}</h1>
+        <h3 class="text-center fs-4 my-2">Band: ${data?.brand || 'No Found'}</h3>
+    `;
+    phoneDetails.appendChild(div);
+
+    // details section
+    const detailsDiv = document.createElement('div');
+    detailsDiv.className = 'container-fluid details-text col-sm-6 my-3';
+    detailsDiv.innerHTML = `
+    <div class="row">
+        <div class="row">
+            <div class="col-sm-4 col-4">
+                <h4>Release Date:</h4>
+            </div>
+            <div class="col-sm-8 col-8">
+                 <p>${data?.releaseDate || 'No release Date found'}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-4">
+                <h4>Display Size:</h4>
+            </div>
+            <div class="col-sm-8 col-8">
+                 <p>${data.mainFeatures?.displaySize || 'No found'}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-4">
+                <h4>ChipSet:</h4>
+            </div>
+            <div class="col-sm-8 col-8">
+                 <p>${data.mainFeatures?.chipSet || 'No found'}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-4">
+                <h4>Memory:</h4>
+            </div>
+            <div class="col-sm-8 col-8">
+                 <p>${data.mainFeatures?.memory || 'No found'}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-4">
+                <h4>Sensors:</h4>
+            </div>
+            <div class="col-sm-8 col-8" id="sensors-div">
+                <p>${data.mainFeatures?.sensors.join(", ") || 'No found'}</p>
+                 
+            </div>
+        </div>
+    </div>
+    `;
+    phoneDetails.appendChild(detailsDiv);
+
+    // display others details 
+    const othersDiv = document.createElement('div');
+    if(data.others !== undefined){
+        othersDiv.innerHTML=`
+        <div class="row">
+        </div>`
+        for (const prop in data.others) {
             const div = document.createElement('div');
-            div.classList.add('col');
-            div.classList.add('card-style')
+            div.className = 'row';
             div.innerHTML = `
-                <div class="card">
-                    <img height= '350px' p-5 src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                    <div class="card-body" style="height:200px; overflow:hidden">
-                        <h5 class="card-title"> <b>Book Name:</b> ${book.title}</h5>
-                        <p><b>Author Name:</b> ${book.author_name ? book.author_name.slice(0, 2) : 'No Author'}</p>
-                        <p><b>Publisher:</b> ${book.publisher ? book.publisher : 'No Publisher'}</p>
-
-                        <p class="card-text"><b>Published year:</b> ${book.first_publish_year ? book.first_publish_year : 'No Published Year'}</p>
-                    </div>
-                </div>
-            `;
-            booksContainer.appendChild(div);
-        });
-
+                    <div class="row">
+                        <div class="col-sm-4 col-4">
+                            <h4 class="text-end">${prop}:</h4>
+                        </div>
+                        <div class="col-sm-8 col-8">
+                             <p>${data?.others[prop] || 'No'}</p>
+                        </div>
+                `;
+            othersDiv.appendChild(div);
+            detailsDiv.appendChild(othersDiv);
+        };
     }
-    spin('none');
+    else{
+        othersDiv.innerHTML ='';
+        detailsDiv.appendChild(othersDiv);
+    }
+    phoneDetails.appendChild(detailsDiv);
+};
 
-}
+// show display result function
+const showDisplay =(data , section) =>{
+    data.forEach(result => {
+        const div = document.createElement('div');
+        div.className = 'col';
+        div.innerHTML = `
+        <div class="card border-0">
+            <img src="${result.image}" class="card-img-top w-50 mx-auto mt-3" alt="${result?.phone_name || 'No Found'}">
+            <div class="card-body text-center">
+                <h5 class="card-title phone-title">${result?.phone_name || 'No Found'}</h5>
+                <p class="card-text mb-2">Band: ${result?.brand ||'No Found'}</p>
+                <button class="btn btn-info details-button" onclick="getDetails('${result.slug}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">See Details</button>
+            </div>
+        </div>
+         `;
+        section.appendChild(div);
+    });
+};
+
+// add enter key button
+document.onkeydown = function(){
+    if(window.event.keyCode === 13){
+        getSearch();
+    }
+};
+
+
